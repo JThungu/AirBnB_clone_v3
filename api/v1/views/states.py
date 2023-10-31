51ad930447d206ed382cd1554b2f0f7baf87b7ce
calebrori <32507245+calebrori@users.noreply.github.com>
2023-10-31 08:53:13 +0300

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,7 +7,7 @@ from flask import jsonify, abort, make_response, request
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
-def states():
+def states(state_id=None):
     """ Retrieves the list of all State objects """
     d_states = storage.all(State)
     return jsonify([obj.to_dict() for obj in d_states.values()])
