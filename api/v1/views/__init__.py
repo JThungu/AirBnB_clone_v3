e3ccfc060526771b690333d5949de68bbfb34a0e
calebrori <32507245+calebrori@users.noreply.github.com>
2023-10-31 17:44:27 +0300

    api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 '''contains the blueprint'''
-from flask import Blueprint
-
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from flask import Blueprint
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.states import *
